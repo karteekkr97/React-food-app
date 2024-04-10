5c3478626a172fdff5afea07d3b94d68bec34124
karteekkr97 <karteekkrshetty97@gmail.com>
2024-04-09 22:02:18 -0700

    Hello world React

---
+++ App.js
@@ -0,0 +1,3 @@
+const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child1"},[React.createElement("h1",{},"im a h1 tag"),React.createElement("h2",{},"im a h2 tag")]),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"im a h1 tag"),React.createElement("h2",{},"im a h2 tag")])])
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
