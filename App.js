7d8a5099d1bfe1f5e7985fefafa25047fc960583
karteekkr97 <karteekkrshetty97@gmail.com>
2024-04-11 09:19:40 -0700

    Episode-2

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
-const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child1"},[React.createElement("h1",{},"im a h1 tag"),React.createElement("h2",{},"im a h2 tag")]),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"im a h1 tag"),React.createElement("h2",{},"im a h2 tag")])])
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "im a h1 tag"),
+    React.createElement("h2", {}, "im a ddd tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "im a h1 tag"),
+    React.createElement("h2", {}, "im a h2 tag"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React
+
+
+# Parcel
+- Dev Build
+- Local server
+- HMR- Hot Module Replacement
+- File watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Compress
+- Bundling
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree Shaking - Remove unused  code
+- Different dev and prod bundles
