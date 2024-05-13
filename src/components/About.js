880deef6b1649bc816a714ec7025cd07f4e35d60
karteekkr97 <karteekkrshetty97@gmail.com>
2024-05-13 10:39:16 -0700

    React food app

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return(
+        <div>
+            <h1>About us</h1>
+            <h2>This is food delivery app</h2>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact=()=>{
+    return(
+        <div>
+            <h1>Hello</h1>
+            <h2>Contact:7676380238</h2>
+        </div>
+    )
+}
+
+export default Contact;
