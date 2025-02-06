d0fc3a95217333b7087dff743d7a942f85998cea
KARTEEK K R <62058343+karteekkr97@users.noreply.github.com>
2025-02-06 15:53:45 +0530

    api

--- functions/proxy.mjs
+++ functions/proxy.mjs
@@ -2,8 +2,7 @@
 import fetch from 'node-fetch';
 
 export async function handler(event, context) {
-  const apiUrl = '
-https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'; // Your live API endpoint
+  const apiUrl = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'; // Your live API endpoint
 
   try {
     const response = await fetch(apiUrl);
