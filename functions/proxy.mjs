1d7dc3884df0c049a5b4c76d5e753f3452e012e2
KARTEEK K R <62058343+karteekkr97@users.noreply.github.com>
2025-02-06 15:52:20 +0530

    api

--- functions/proxy.mjs
+++ functions/proxy.mjs
@@ -2,7 +2,8 @@
 import fetch from 'node-fetch';
 
 export async function handler(event, context) {
-  const apiUrl = 'https://api.swiggy.com/api/restaurants/list'; // Your live API endpoint
+  const apiUrl = '
+https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'; // Your live API endpoint
 
   try {
     const response = await fetch(apiUrl);
@@ -10,6 +11,11 @@ export async function handler(event, context) {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Allow all origins
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify(data),
     };
   } catch (error) {
