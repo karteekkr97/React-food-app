d871134f605a4dcff9f23f5ab71d685146cc1035
KARTEEK K R <62058343+karteekkr97@users.noreply.github.com>
2025-02-07 10:49:54 +0530

    live api cors issue resolved

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,27 +15,34 @@ const Body = () => {
 
   const fetchData = async () => {
     try {
-      const apiUrl =
-      process.env.REACT_APP_API_BASE_URL ||
-      (window.location.hostname === "localhost"
-        ? "http://localhost:5000/api/restaurants/list"
-        : "/.netlify/functions/proxyRestaurants");
+      const data = await fetch(
+        "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
+
+      const json = await data.json();
 
 
-      const response = await fetch(apiUrl);
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+      // was showing an error of data fatching because sometime data coming from cards[1] sometime cards[2] and different on other times so me make a function and check which value of i gives data in cards[i]
+      async function checkJsonData(jsonData) {
+
+        for (let i = 0; i < jsonData?.data?.cards.length; i++) {
+
+          // initialize checkData for Swiggy Restaurant data
+          let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+          // if checkData is not undefined then return it
+          if (checkData !== undefined) {
+            return checkData;
+          }
+        }
       }
+      // call the checkJsonData() function which return Swiggy Restaurant data
+      const resData = await checkJsonData(json);
 
-      const json = await response.json();
-      setListOfRestaurants(
-        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || []
-      );
-      setFilteredRestaurants(
-        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || []
-      );
+      // update the state variable restaurants with Swiggy API data
+      setListOfRestaurants(resData);
+      setFilteredRestaurants(resData);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.log(error);
     }
   };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,4 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL = "https://icons.iconarchive.com/icons/sonya/swarm/256/Fast-Food-icon.png";
 
-export const MENU_API = process.env.REACT_APP_MENU_API_URL || (window.location.hostname === "localhost"
-    ? "http://localhost:5000/api/menu?restaurantId="
-    : "/.netlify/functions/proxyRestaurants")
+export const MENU_API = "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
